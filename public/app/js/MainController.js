70f9d3374bc6cba8478b863c523703f261adaf31
Anatoliy A Aksenov <anatoliy@aksenov.org>
2017-02-16 22:26:02 +0300

    Move to timeline style

--- public/app/js/MainController.js
+++ public/app/js/MainController.js
@@ -5,35 +5,84 @@
     .module('App')
     .controller('MainController', MainController);
 
-    MainController.inject = ['$scope', 'dataAssistant'];
+    MainController.inject = ['$scope', '$location','$anchorScroll', 'dataAssistant'];
 
-    function MainController($scope, dataAssistant) {
-		$scope.user = {};
-
-		$scope.profile = function(){
-			dataAssistant.get('/.auth/me').then(function(data){
-				$scope.user = data;
-				$scope.page = 'profile';
-				console.log('user:' + JSON.stringify(data));
-			}, function(error){
-				$scope.user = {};
-				$scope.user_error = error;
-				$scope.page = 'news';
-			});
-		};		
-      
-		$scope.showProfile = function(){
-			$scope.page = 'profile';
+    function MainController($scope, $location, $anchorScroll, dataAssistant) {
+			$scope.step = 1;
+			$scope.user = {};
+			$scope.saved ={};
 			
-		};
+			$scope.init = function(){
+				console.log('begin init');
+				dataAssistant.get('/api/userdata')
+				.then(function(data){
+					console.log(JSON.stringify(data));
+					$scope.saved = data.data;
+					$scope.step = $scope.saved.step;					
+					$scope.cardnumber = $scope.saved.cardnumber;
+					$scope.code = $scope.saved.code;
 
-		$scope.showLogin = function(){
-			$scope.page = 'login';
-		};
+					$scope.setStep($scope.step);
+					
+				}).then(function(error){
+					console.log(JSON.stringify(error));
+				});
+			};
 
-		$scope.showNews = function(){
-			$scope.page = 'news';
-		}
-						
+			$scope.save = function(){
+			   var user = {};
+				 user.firstname = $scope.user.firstname;
+				 user.lastname = $scope.user.lastname;
+				 user.middlename = $scope.user.middlename;
+				 user.passport = $scope.user.passport;
+				 user.phonenumber = $scope.user.phonenumber;
+				 
+				 //check data!
+				 
+				 dataAssistant.post('/api/userdata/save', user)
+				 .then(function(data){
+					 $scope.saved = user;
+					 $scope.setStep(2);
+				
+           console.log(JSON.stringify(data));
+				 })
+				 .then(function(error){
+						console.log(JSON.stringify(error));
+				 });
+			};
+
+			$scope.savecard = function(){
+				var cardnumber = $scope.user.cardnumber;
+				dataAssistant.post('/api/userdata/card/add', {cardnumber: cardnumber})
+				.then(function(data){
+					$scope.saved.cardnumber = cardnumber;
+				  $scope.setStep(4);
+					console.log(JSON.stringify(data));
+				})
+				.then(function(error){
+					console.log(JSON.stringify(error));
+				})
+			}
+
+			$scope.getCard = function(){
+				dataAssistant.get('/api/userdata/card')
+				.then(function(data){
+					$scope.cardnumber = data.cardnumber;
+					$scope.setStep(3);					
+				})
+				.then(function(error){
+					console.log(JSON.stringify(error));
+				});
+
+			}
+				
+			$scope.setStep = function(step){
+				$scope.step = step;
+				$scope.user.step = step;			
+				var stephash = `step${step}`;
+				console.log();
+				$location.hash(stephash);
+				$anchorScroll();	
+			};						
     }
 })();
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var helmet = require('helmet');
 
 // Create a new Express application.
 var app = express();
@@ -7,25 +7,81 @@ var app = express();
 var port = process.env.port || 8080;
 var address = process.env.address || '0.0.0.0';
 
+var cookieSession = require('cookie-session');
+
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'Microsoft BlockchainSckool darkchain', resave: true, saveUninitialized: true }));
+app.use(require('body-parser').json());
+
+// app.use(require('express-session')({ 
+//   secret: 'RosEuroBank developers the best of the best!', 
+//   resave: true, 
+//   saveUninitialized: true,
+//   name : 'sessionId',
+//   maxAge: 24 * 60 * 60 * 1000
+// }));
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['kkkey  1','iejdkj3 2'],
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 
 
 //authrequest
 //http://192.168.99.100:8081/authorize?client_id=222222&redirect_uri=http%3A%2F%2F192.168.99.100%3A8089%2Foauth2&response_type=code&scope=all&state=xyz
 
-//public folder 
-app.use('/', express.static('public/app'));
+app.use(helmet());
+app.disable('x-powered-by');
 
-app.get('/oauth2',
+app.use('/',express.static('public/app'));
+
+app.post('/api/userdata/save', function(req, res){
+  var firstname = req.body.firstname;
+  var middlename = req.body.middlename;
+  var lastname = req.body.lastname;
+  var passport = req.body.passport;
+  var user = {};
+  user.firstname = firstname;
+  user.middlename = middlename;
+  user.lastname = lastname;
+  user.passport = passport;
+  user.step = 2;
+  req.session.user = user;
+  res.status(200).json({result: "Success"});
+});
+
+app.get('/api/userdata', function(req, res){
+  var user = req.session.user;
+  res.status(200).json(user);
+});
+
+app.post('/api/userdata/card/add', function(req, res){
+  var cardnumber = req.body.cardnumber;
+  req.session.user.cardnumber = cardnumber;
+  req.session.step = 3;
+  req.session.user.step = 3;
+  res.status(200).json({result: "Success"});
+});
+
+app.get('/api/userdata/card', function(req, res){
+  var cardnumber = req.session.user.cardnumber;
+  res.status(200).json({cardnumber: cardnumber});
+});
+
+app.get('/oauthcomplete',
   function(req, res){
     var code = req.originalUrl;
-    var r = /code=\w+/;
-    if (r.test(code)){
+    var r = /code=(\w+)/;
 
+    if (r.test(code)){
+      req.session.user.step = 3;
+      req.session.authcode = req.originalUrl.match(r)[1];
       res.redirect("/?login=success");
     } else {
+      req.session.user.step = 2;
+      req.session.authcode = null;
       res.redirect('/?login=false');
     }
 
@@ -36,5 +92,6 @@ app.get('/api/list',
     res.status(200).json({list: 'list'});
   });
 
-
-app.listen(port);
+app.listen(port, function(){
+  console.log(`listening localhost:${port}`);
+});
